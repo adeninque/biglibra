61c0d519deac9f0c1aa029e3b2d0d34295ede3e9
adeninque <adilmarsov5@gamil.com>
2023-03-29 10:35:29 +0600

    first models created

--- libra/models.py
+++ libra/models.py
@@ -2,13 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-  pass
+  title = models.CharField(max_length=255)
+  slug = models.SlugField(unique=True)
+  cover = models.ImageField(upload_to='cover/%y/%m/%d')
+  authors = models.ManyToManyField('Authors')
 
 class Borrow(models.Model):
-  pass
+  when = models.DateField(auto_now_add=True)
+  deadline = models.DateField()
+  book = models.ForeignKey('Authors', on_delete=models.PROTECT)
 
 class Authors(models.Model):
-  pass
+  photo = models.ImageField('author_photos/%Y/%m/%d', default='defs/profile_default.svg')
+  slug = models.SlugField(unique=True)
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
 
 class Categories(models.Model):
-  pass
+  name = models.CharField(max_length=255)
+  slug = models.SlugField(unique=True)
