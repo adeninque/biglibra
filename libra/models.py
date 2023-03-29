482a39bc1169f36cff31b084f488986aad48476c
adeninque <adilmarsov5@gamil.com>
2023-03-29 10:03:31 +0600

    first commit

---
+++ libra/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+  pass
+
+class Borrow(models.Model):
+  pass
+
+class Authors(models.Model):
+  pass
+
+class Categories(models.Model):
+  pass
