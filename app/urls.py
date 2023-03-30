8b9a2843ee5519ab07fb3e86202df5f7d8206b3c
ilvus <azamat.ibragiomov3102@gmail.com>
2023-03-30 10:45:59 +0600

    app/templates updated

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', Home.as_view(), name='home')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, DeleteView, TemplateView
+
 # Create your views here.
 def home_page(request):
     return HttpResponse('Hello')
+
+
+class Home(TemplateView):
+    template_name = 'app/home.html'
