7f71062fce5b80d0b839242b09087f4c4096ff66
adeninque <adilmarsov5@gamil.com>
2023-03-30 11:18:08 +0600

    remote db added

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',                      
+        'USER': 'postgres',
+        'PASSWORD': 'biglibradb1234',
+        'HOST': 'db.ewvsydxrvydsdfdlcpab.supabase.co',
+        'PORT': '5432',
     }
 }
 
