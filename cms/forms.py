7f534ba8406fe331254dd18f1af9bc555d3f23fd
adeninque <adilmarsov5@gamil.com>
2023-04-05 18:31:48 +0600

    1 task complited fully

---
+++ cms/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+
+from libra.models import (
+  Book,
+  Borrow,
+  Authors,
+  Categories
+)
+
+
+class BookForm(forms.ModelForm):
+  class Meta:
+    model = Book
+    fields = ('title', 'copies', 'cover', 'authors')
+    widgets = {
+      'title': forms.TextInput(attrs={'class': 'form-control'}),
+      'copies': forms.NumberInput(attrs={'class': 'form-control'}),
+      'authors': forms.SelectMultiple(attrs={'class': 'form-select'}),
+      'cover': forms.FileInput(attrs={'style': 'height: 100%; display: inline-block;'}),
+    }
