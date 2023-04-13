2ff010cc7b5021c1c982933906693de1eb3230d4
ilvus <azamat.ibragiomov3102@gmail.com>
2023-04-13 11:02:35 +0600

    templates and views updated

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', BookHome.as_view(), name='home'),
-    path('book/<slug:slug>/', BookDetail.as_view(), name='book')
+    path('book/<slug:slug>/', BookDetail.as_view(), name='book'),
+    path('login/', Login.as_view(), name='login')
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, DeleteView, TemplateView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from libra.models import Book
 # Create your views here.
 from libra.views import BookListView
 
 menu = [
-    {'url': 'home', 'name': 'Home'}
+    {'url': 'home', 'name': 'Home'},
+    {'url': 'login', 'name': 'Login'}
 ]
 
 
@@ -36,8 +38,12 @@ class BookDetail(DetailView):
     model = Book
     template_name = 'app/book.html'
     context_object_name = 'book'
-
+    extra_context = {'title': 'Book'}
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({'menu': menu})
         return context
+
+class Login(LoginView):
+
+    template_name = 'app/authorization.html'
